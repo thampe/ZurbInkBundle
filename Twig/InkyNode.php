db4e83c0fd16087f4f247fede9ec0f6409e438ff
Andreas Braun <git@alcaeus.org>
2016-10-25 07:34:58 +0200

    Ensure forward compatibility with Twig 2.0
    Referencing to an extension by its name is deprecated and will be removed in Twig 2.0. To avoid raising the Twig requirement, the node now checks if the twig version in question still supports getting an extension by name.

--- Twig/InkyNode.php
+++ Twig/InkyNode.php
@@ -11,19 +11,17 @@
  *  @date       16.04.16
  */ 
 
-
 namespace Hampe\Bundle\ZurbInkBundle\Twig;
-
 
 use Twig_Node;
 
 class InkyNode extends Twig_Node
 {
-
     public function __construct(\Twig_Node $body, $lineno, $tag = 'inky')
     {
         parent::__construct(array('body' => $body), array(), $lineno, $tag);
     }
+
     /**
      * Compiles the node to PHP.
      *
@@ -31,12 +29,14 @@ class InkyNode extends Twig_Node
      */
     public function compile(\Twig_Compiler $compiler)
     {
+        $extensionName = (version_compare(\Twig_Environment::VERSION, '1.26.0') >= 0) ? 'Hampe\Bundle\ZurbInkBundle\Twig\InkyExtension' : InkyExtension::NAME;
+
         $compiler
             ->addDebugInfo($this)
             ->write('ob_start();' . PHP_EOL)
             ->subcompile($this->getNode('body'))
             ->write('$inkyHtml = ob_get_clean();' . PHP_EOL)
-            ->write('echo $this->env->getExtension(\'zurb_ink.inky\')->parse($inkyHtml);' . PHP_EOL);
+            ->write("echo \$this->env->getExtension('{$extensionName}')->parse(\$inkyHtml);" . PHP_EOL);
     }
 
 }
